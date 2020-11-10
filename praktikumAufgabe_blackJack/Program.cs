b1a80b8d3111c0fcb1ed70318fa3a741295b92f7
Eyüp Coskun <eyupcoskun.de@gmail.com>
2020-11-10 22:29:37 +0100

    letzte version

--- praktikumAufgabe_blackJack/Program.cs
+++ praktikumAufgabe_blackJack/Program.cs
@@ -8,116 +8,109 @@ namespace praktikumAufgabe_blackJack
         static void Main(string[] args)
         {
             string spieler;
-            int anzspielerchips = 10;
+            int anzSpielerChips = 10;
             int spielerKarteSumme = 0;
             int computerKarteSumme = 0;
-
-
-            Console.WriteLine("Dein Guthaben: " + anzspielerchips);
-
-            int spielerEinsatz;
-
-            // Einsatz
-            Console.Write("Dein Einsatz: ");
-            spielerEinsatz = Convert.ToInt32(Console.ReadLine());
-            if (spielerEinsatz < 1 || spielerEinsatz > 10)
+            bool nochmal = true;
+            while (anzSpielerChips > 0 && nochmal)
             {
-                Console.WriteLine("Du musst nindestens 1 und kannst max. 10 Chips setzen.");
-            }
-            else
-            {
-
-                //random für erste  karte bekommen
-                Random zufallkarte = new Random();
-                int karte;
-                karte = zufallkarte.Next(2, 12);
-                Console.Write("Karte: " + karte + "           ");
-                spielerKarteSumme += karte;
-                Console.WriteLine("Gesamt: " + spielerKarteSumme);
-
-                //noch eine karte
-
-                karte = zufallkarte.Next(2, 12);
-                Console.Write("Karte: " + karte + "           ");
-                spielerKarteSumme += karte;
-                Console.WriteLine("Gesamt: " + spielerKarteSumme);
-
-
-
-                //weitere für eine karte bekommen
-                bool weiter = true;
-                while (spielerKarteSumme < 21 && weiter)
+                Console.WriteLine("Dein Guthaben: " + anzSpielerChips+" Chips");
+                int spielerEinsatz;
+                // Einsatz
+                Console.Write("Dein Einsatz: ");
+                spielerEinsatz = Convert.ToInt32(Console.ReadLine());
+                if (spielerEinsatz < 1 || spielerEinsatz > 10)
                 {
-                    Console.WriteLine("noch eine karte (j | n)");
-                    string
-                        answer = Console.ReadLine();
-                    if (answer == "j")
-                    {
-                        karte = zufallkarte.Next(2, 12);
-                        Console.Write("Karte: " + karte + "           ");
-                        spielerKarteSumme += karte;
-                        Console.WriteLine("Gesamt: " + spielerKarteSumme);
-                        weiter = true;
-                    }
-                    else
-                    {
-                        weiter = false;
-                    }
-
-
-
+                    Console.WriteLine("Du musst nindestens 1 und kannst max. 10 Chips setzen.");
                 }
-                if (spielerKarteSumme < 21)
+                else
                 {
-
-                    while (computerKarteSumme < 16)
+                    //random für erste  karte bekommen
+                    Random zufallkarte = new Random();
+                    int karte;
+                    karte = zufallkarte.Next(2, 12);
+                    Console.Write("Karte: " + karte + "           ");
+                    spielerKarteSumme += karte;
+                    Console.WriteLine("Gesamt: " + spielerKarteSumme);
+                    //zweite Karte
+                    karte = zufallkarte.Next(2, 12);
+                    Console.Write("Karte: " + karte + "           ");
+                    spielerKarteSumme += karte;
+                    Console.WriteLine("Gesamt: " + spielerKarteSumme);
+                    //weitere eine karte bekommen, wenn Spieler möchte
+                    bool weiter = true;
+                    while (spielerKarteSumme < 21 && weiter)
                     {
-
-                      
-                        karte = zufallkarte.Next(2, 12);
-                        Console.Write("Karte: " + karte + "           ");
-                        computerKarteSumme += karte;
-                        Console.WriteLine("Gesamt Computer: " + computerKarteSumme);
-
+                        Console.WriteLine("noch eine karte (j | n)");
+                        string answer = Console.ReadLine();
+                        if (answer == "j")
+                        {
+                            karte = zufallkarte.Next(2, 12);
+                            Console.Write("Karte: " + karte + "           ");
+                            spielerKarteSumme += karte;
+                            Console.WriteLine("Gesamt: " + spielerKarteSumme);
+                            weiter = true;
+                        }
+                        else
+                        {
+                            weiter = false;
+                        }
                     }
-
-
-
-                }
-                //wer gewonnen
-                if (spielerKarteSumme > 21)
-                {
-                    Console.WriteLine("ich habe gewonnen");
-                    anzspielerchips -= spielerEinsatz;
-                    Console.WriteLine("Dein Guthaben: " + anzspielerchips);
-
-                }
-
-                else if (computerKarteSumme > 21)
-                {
-                    Console.WriteLine("Du hast gewonnen");
-                    anzspielerchips += spielerEinsatz;
-                    Console.WriteLine("Dein Guthaben: " + anzspielerchips);
-                }
-                else if (spielerKarteSumme <= 21 && computerKarteSumme <= 21)
-                {
-                    if (computerKarteSumme >= spielerKarteSumme)
+                    //falls Spilerkartesumme <21 ist, dann spielt der Computer
+                    if (spielerKarteSumme < 21)
                     {
-                        Console.WriteLine("Ich habe gewonnen");
-                        anzspielerchips -= spielerEinsatz;
-                        Console.WriteLine("Dein Guthaben: " + anzspielerchips);
+                        while (computerKarteSumme < 16)
+                        {
+                            karte = zufallkarte.Next(2, 12);
+                            Console.Write("Karte: " + karte + "           ");
+                            computerKarteSumme += karte;
+                            Console.WriteLine("Gesamt Computer: " + computerKarteSumme);
+                        }
+                        Console.WriteLine(); Console.WriteLine();
                     }
-                    else
+                    Console.WriteLine(); Console.WriteLine();
+                    //Entscheidung, wer gewonnen hat
+                    if (spielerKarteSumme > 21)
+                    {
+                        Console.WriteLine("ich habe gewonnen");
+                        anzSpielerChips -= spielerEinsatz;
+                        Console.WriteLine("Dein Guthaben: " + anzSpielerChips);
+                    }
+                    else if (computerKarteSumme > 21)
                     {
                         Console.WriteLine("Du hast gewonnen");
-                        anzspielerchips += spielerEinsatz;
-                        Console.WriteLine("Dein Guthaben: " + anzspielerchips);
+                        anzSpielerChips += spielerEinsatz;
+                        Console.WriteLine("Dein Guthaben: " + anzSpielerChips+ "chips.");
+                    }
+                    else if (spielerKarteSumme <= 21 && computerKarteSumme <= 21)
+                    {
+                        if (computerKarteSumme >= spielerKarteSumme)
+                        {
+                            Console.WriteLine("Ich habe gewonnen");
+                            anzSpielerChips -= spielerEinsatz;
+                            Console.WriteLine("Dein Guthaben: " + anzSpielerChips);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Du hast gewonnen");
+                            anzSpielerChips += spielerEinsatz;
+                            Console.WriteLine("Dein Guthaben: " + anzSpielerChips);
+                        }
                     }
                 }
-
-
+                Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+                //Abfrage,ob Spieler weiter macht oder nicht
+                Console.WriteLine("Nochmal (j/n)");
+                string answer2 = Console.ReadLine();
+                if (answer2 == "j")
+                {
+                    nochmal = true;
+                    spielerKarteSumme = 0;
+                    computerKarteSumme = 0;
+                }
+                else
+                    nochmal = false;
             }
-
         }
     }
 }
